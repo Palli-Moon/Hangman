68ee54c31dea675895722eb1f6056e2f35b44e89
Palli-Moon <pallimoon@gmail.com>
2014-08-05 18:28:44 +0000

    Hotfix to main.cpp, filestream is not yet connected to game

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main()
 {
-    //Hangman h;
-    //h.start_game();
-    char arr[] = "asdf.txt";
-    Files f(arr);
-    cout << f.get_string(3) << endl;
-    cout << f.get_string(1) << endl;
-    cout << f.get_string(998) << endl;
+    // TODO: CONNECT TO FILESTREAM!!!!
+    Hangman h;
+    h.start_game();
+    //char arr[] = "asdf.txt";
+    //Files f(arr);
+    //cout << f.get_string(3) << endl;
+    //cout << f.get_string(1) << endl;
+    //cout << f.get_string(998) << endl;
     return 0;
 }
