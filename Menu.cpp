1f910749d1ddbe91417d1bfc894d3f01143fd9a3
Palli-Moon <pallimoon@gmail.com>
2014-08-15 13:59:29 +0000

    Adjusted welcome screen

--- Menu.cpp
+++ Menu.cpp
@@ -1,17 +1,24 @@
 #include "Menu.h"
+#include "Hangman.h"
 
 void Menu::welcome_screen()
 {
-    cout << "========================================" << endl;
-    cout << "    Welcome to Hangman by Palli Moon    " << endl;
-    cout << "========================================" << endl;
+    Hangman::clear_screen();
+    cout << "            ========================================" << endl;
+    cout << "                Welcome to Hangman by Palli Moon    " << endl;
+    cout << "            ========================================" << endl;
     cout << endl;
-    cout << "To play, enter the letter you would like" << endl;
-    cout << "guess and press ENTER. It is possible to" << endl;
-    cout << "guess more than 1 letter at a time, but " << endl;
-    cout << "it is not recommended." << endl;
-    cout << "ended." << endl;
+    cout << "            To play, enter the letter you would like" << endl;
+    cout << "            guess and press ENTER. It is possible to" << endl;
+    cout << "            guess more than 1 letter at a time. How-" << endl;
+    cout << "            ever, doing so is not recommended." << endl;
     cout << endl;
-    cout << "Press ENTER to continue" << endl;
+    cout << "            Press ENTER to continue" << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
     cin.get();
 }
